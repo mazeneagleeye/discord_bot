ad105d3d37d666aca22985b1d050aa83933d7415
mazeneagleeye <mazenabosenna15@gmail.com>
2025-12-23 14:14:38 +0200

    Add files via upload

---
+++ clanwar-bot/deploy-commands.js
@@ -0,0 +1,26 @@
+const { REST, Routes } = require("discord.js");
+const { clientId, guildId, token } = require("./config.json");
+const fs = require("fs");
+
+const commands = [];
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: "10" }).setToken(token);
+
+(async () => {
+  try {
+    console.log("⏳ Refreshing slash commands...");
+    await rest.put(
+      Routes.applicationCommands(clientId),
+      { body: commands }
+    );
+    console.log("✅ Slash commands registered!");
+  } catch (error) {
+    console.error(error);
+  }
+})();
